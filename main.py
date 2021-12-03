ea838df21d6b319446aa6b98593ad531bdaa99ad
WhyBangad <byatin2005@gmail.com>
2021-12-03 20:57:01 +0530

    Refactored code and added classes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pygame
-import time, math
+import pygame, math
 
 # TODO:
 # Difficulty level adjusts vertical speed
@@ -15,122 +14,174 @@ screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption('Brick Breaker')
 pygame.display.set_icon(pygame.image.load('./img/joystick.png'))
 
-BALL_SIZE = 32
-BALL_LOC = (400, 300)
-BALL_VEL = [0.3, 1]
-BALL_CENTRE = 32*math.sqrt(2)
-ball = pygame.image.load('./img/ball.png')
-ball_coords = BALL_LOC
-ball_velocity = BALL_VEL
-def load_ball(coords):
-    screen.blit(ball, coords)
+class Ball():
+    # change ball's velocity in Ball object
+    SIZE = 32
+    LOC = (400, 300)
+    VEL = [0.3, 1]
+    CENTRE = 32*math.sqrt(2)
+    BNDRY_H = SCREEN_SIZE[0] - SIZE
+    BNDRY_V = SCREEN_SIZE[1] - SIZE
+    MARGIN_V = 20
+    ball = pygame.image.load('./img/ball.png')
 
-brick = pygame.image.load('./img/brick.png')
-BRICK_SIZE = 64
-BRICK_CENTRE = 32
-BRICK_ROWS = 3
-BRICK_COLS = 12
-MARGIN_H = (SCREEN_SIZE[0] - BRICK_COLS*BRICK_SIZE)/2
-MARGIN_V = 20
-BNDRY_H = SCREEN_SIZE[0] - BALL_SIZE
-BNDRY_V = SCREEN_SIZE[1] - BALL_SIZE
-broken = [[False for _ in range(BRICK_COLS)] for _ in range(BRICK_ROWS)]
-broken_count = 0
-def load_bricks():
-    for i in range(BRICK_ROWS):
-        for j in range(BRICK_COLS):
-            if not broken[i][j]:
-                screen.blit(brick, (MARGIN_H + j*BRICK_SIZE, MARGIN_V + i*BRICK_SIZE))
+    def __init__(self):
+        # add option to pass ball location and image
+        self.coords = self.LOC
+        self.velocity = self.VEL
 
-def brick_bounce(br_coords, bl_coords):
-    # determine which surface is being hit
-    # left, right, top, bottom
-    # horizontal, vertical
-    hit = [False, False]
-    if bl_coords[0] == br_coords[0]:
-        hit[0] = True
-    elif bl_coords[1] == br_coords[1]:
-        hit[1] = True
-    else:
-        slope = (br_coords[1]-bl_coords[1])/(br_coords[0]-bl_coords[0])
-        if abs(slope) == 1:
-            hit[0] = hit[1] = True
+    def load(self):
+        screen.blit(Ball.ball, self.coords)
+
+    def update(self):
+        if self.coords[0] + self.velocity[0] >= Ball.BNDRY_H  or self.coords[0] + self.velocity[0] < 0:
+            self.velocity[0] *= -1
+        if self.coords[1] + self.velocity[1] < Ball.MARGIN_V:
+            self.velocity[1] *= -1
+        self.coords = (self.coords[0] + self.velocity[0], self.coords[1] + self.velocity[1])
+
+class Brick():
+    brick = pygame.image.load('./img/brick.png')
+    SIZE = 64
+    CENTRE = 32
+    CORNER_SLOPE = 0.5
+
+    def __init__(self, position:tuple, visible:bool) -> None:
+        self.coords = position
+        self.vis = visible
+
+    def load(self):
+        if self.vis:
+            global screen
+            screen.blit(Brick.brick, self.coords)
+
+    def hit(self):
+        self.vis = False
+
+    def visible(self):
+        return self.vis
+
+    def bounce(self, ball:Ball):
+        curr_x, curr_y = (self.coords[0]+self.CENTRE, self.coords[1]+self.CENTRE)
+        ball_x, ball_y = (ball.coords[0] + ball.CENTRE, ball.coords[1] + ball.CENTRE)
+        # determine which surface is being hit
+        # horizontal, vertical
+        hit = [False, False]
+        if curr_x == ball_x:
+            hit[0] = True
+        elif curr_y == ball_y:
+            hit[1] = True
         else:
+            # corner hit should change speed in both directions
+            slope = (curr_y-ball_y)/(curr_x-ball_x)
             if abs(slope) > 1:
                 hit[0] = True
             else:
                 hit[1] = True
-    global ball_velocity
-    if hit[0]:
-        ball_velocity[1] *= -1
-    if hit[1]:
-        ball_velocity[0] *= -1
-        
+        if hit[0]:
+            ball.velocity[1] *= -1
+        if hit[1]:
+            ball.velocity[0] *= -1
 
-def update_bricks(coords):
-    for i in range(BRICK_ROWS):
-        for j in range(BRICK_COLS):
-            curr_x, curr_y = MARGIN_H + j*BRICK_SIZE, MARGIN_V + i*BRICK_SIZE
-            if not broken[i][j] and coords[0]+BALL_SIZE/2 >= curr_x and coords[0]-BALL_SIZE/2 <= curr_x + BRICK_SIZE and coords[1]+BALL_SIZE/2 >= curr_y and coords[1]-BALL_SIZE/2 <= curr_y + BRICK_CENTRE:
-                broken[i][j] = True
-                global broken_count
-                broken_count += 1
-                brick_bounce((curr_x+BRICK_CENTRE, curr_y+BRICK_CENTRE), (coords[0] + BALL_CENTRE, coords[1] + BALL_CENTRE))
+class Grid():
+    ROWS = 3
+    COLS = 12
+    MARGIN_H = (SCREEN_SIZE[0] - COLS*Brick.SIZE)/2
+    MARGIN_V = 20
 
-platform = pygame.image.load('./img/platform.png')
-PLATFORM_LOC = (400, 550)
-PLATFORM_VEL = [0, 0]
-VEL_DEGRADE = -0.002
-PLATFORM_SIZE = 64
-platform_coords = PLATFORM_LOC
-platform_velocity = PLATFORM_VEL
-def load_platform(coords):
-    screen.blit(platform, coords)
+    def __init__(self):
+        self.grid = [[Brick(position=(Grid.MARGIN_H + j*Brick.SIZE, Grid.MARGIN_V + i*Brick.SIZE), visible=True) for j in range(Grid.COLS)] for i in range(Grid.ROWS)]
+        self.broken = 0
 
-def platform_bounce():
-    global ball_coords, platform_coords
-    if ball_coords[0]+BALL_SIZE/2 >= platform_coords[0] and ball_coords[0]-BALL_SIZE/2 <= platform_coords[0]+PLATFORM_SIZE and ball_coords[1]+BALL_SIZE/2>= platform_coords[1]+PLATFORM_SIZE/4:
-        ball_velocity[1] *= -1
-        ball_velocity[0] = ball_velocity[0]/2 + platform_velocity[0]/5
+    def empty(self) -> bool:
+        return self.broken == Grid.ROWS*Grid.COLS
 
-font = pygame.font.Font('./basketball-font/Basketball.otf', 30)
-SCORE_COORDS = (350,5)
-def load_score(score):
-    text = font.render('Score : ' + str(score), True, (0,0,0))
-    screen.blit(text, SCORE_COORDS)
+    def load(self):
+        for i in range(Grid.ROWS):
+            for j in range(Grid.COLS):
+                self.grid[i][j].load()
 
-RESULT_DISPLAY_X = 310
-RESULT_DISPLAY_Y = 400
-TEXT_DISPLAY_X = 255
-TEXT_DISPLAY_Y = 430
-SHIFT = 50
-background = pygame.transform.scale(pygame.image.load('./img/brick-background.jpg'), (800,600))
-def game_over(status):
-    time.sleep(1)
-    img_size = 256
-    result = []
-    screen.fill((0,0,0))
-    screen.blit(background, (0,0))
-    if status == 'W':
-        result.append((font.render('Congratulations, You won!', True, (0,0,0)), (TEXT_DISPLAY_X-SHIFT/2, RESULT_DISPLAY_Y)))
-        margin = (SCREEN_SIZE[0]-img_size)/2, (SCREEN_SIZE[1]-img_size)/2 - SHIFT
-        screen.blit(pygame.image.load('./img/success.png'), margin)
-    elif status == 'L':
-        result.append((font.render(f'Final score : {broken_count}', True, (0,0,0)), (RESULT_DISPLAY_X, RESULT_DISPLAY_Y)))
-        result.append((font.render('Better luck next time!', True, (0,0,0)), (TEXT_DISPLAY_X, TEXT_DISPLAY_Y)))
-        margin = (SCREEN_SIZE[0]-img_size)/2, (SCREEN_SIZE[1]-img_size)/2 - SHIFT
-        screen.blit(pygame.image.load('./img/game-over.png'), margin)
-    else:
-        # for timeout
-        pass
-    screen.blits(result)
-    pygame.display.update()
-    run = True
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
-                run = False
+    def update(self, ball:Ball):
+        for i in range(Grid.ROWS):
+            for j in range(Grid.COLS):
+                brick = self.grid[i][j]
+                curr_x, curr_y = brick.coords
+                if brick.visible() and ball.coords[0]+ball.SIZE/2 >= curr_x and ball.coords[0]-ball.SIZE/2 <= curr_x + brick.SIZE and ball.coords[1]+ball.SIZE/2 >= curr_y and ball.coords[1]-ball.SIZE/2 <= curr_y + brick.CENTRE:
+                    brick.hit()
+                    self.broken += 1
+                    brick.bounce(ball=ball)
+                    break
 
+class Platform():
+    platform = pygame.image.load('./img/platform.png')
+    LOC = (400, 550)
+    VEL = [0, 0]
+    VEL_DEGRADE = -0.002
+    SIZE = 64
+    BNDRY_H = SCREEN_SIZE[0] - SIZE
+    BNDRY_V = SCREEN_SIZE[1] - SIZE
+
+    def __init__(self):
+        self.coords = Platform.LOC
+        self.velocity = Platform.VEL
+
+    def load(self):
+        screen.blit(self.platform, self.coords)
+
+    def bounce(self, ball:Ball):
+        if ball.coords[0]+ball.SIZE/2 >= self.coords[0] and ball.coords[0]-ball.SIZE/2 <= self.coords[0]+self.SIZE and ball.coords[1]+ball.SIZE/2 >= self.coords[1]+self.SIZE/4:
+            ball.velocity[1] *= -1
+            ball.velocity[0] = ball.velocity[0]/2 + self.velocity[0]/5
+    
+    def update(self):
+        if self.coords[0] + self.velocity[0] >= self.BNDRY_H  or self.coords[0] + self.velocity[0] <= 0:
+            self.velocity[0] *= -1
+        self.coords = (self.coords[0] + self.velocity[0], self.coords[1] + self.velocity[1])
+
+class Window():
+    SCORE_COORDS = (350,5)
+    RESULT_DISPLAY_X = 310
+    RESULT_DISPLAY_Y = 400
+    TEXT_DISPLAY_X = 255
+    TEXT_DISPLAY_Y = 430
+    SHIFT = 50
+    font = pygame.font.Font('./basketball-font/Basketball.otf', 30)
+    background = pygame.transform.scale(pygame.image.load('./img/brick-background.jpg'), (800,600))
+    def load(self, score:int) -> None:
+        screen.blit(Window.background, (0,0))
+        text = Window.font.render('Score : ' + str(score), True, (0,0,0))
+        screen.blit(text, Window.SCORE_COORDS)
+
+    def game_over(self, status:str, broken_count:int) -> None:
+        time.sleep(1)
+        img_size = 256
+        result = []
+        screen.fill((0,0,0))
+        screen.blit(self.background, (0,0))
+        if status == 'W':
+            result.append((self.font.render('Congratulations, You won!', True, (0,0,0)), (Window.TEXT_DISPLAY_X-Window.SHIFT/2, Window.RESULT_DISPLAY_Y)))
+            margin = (SCREEN_SIZE[0]-img_size)/2, (SCREEN_SIZE[1]-img_size)/2 - Window.SHIFT
+            screen.blit(pygame.image.load('./img/success.png'), margin)
+        elif status == 'L':
+            result.append((Window.font.render(f'Final score : {broken_count}', True, (0,0,0)), (Window.RESULT_DISPLAY_X, Window.RESULT_DISPLAY_Y)))
+            result.append((Window.font.render('Better luck next time!', True, (0,0,0)), (Window.TEXT_DISPLAY_X, Window.TEXT_DISPLAY_Y)))
+            margin = (SCREEN_SIZE[0]-img_size)/2, (SCREEN_SIZE[1]-img_size)/2 - Window.SHIFT
+            screen.blit(pygame.image.load('./img/game-over.png'), margin)
+        else:
+            # for timeout
+            pass
+        screen.blits(result)
+        pygame.display.update()
+        run = True
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
+                    run = False
+
+ball = Ball()
+grid = Grid()
+platform = Platform()
+window = Window()
 run = True
 while run:
     press = False
@@ -139,38 +190,31 @@ while run:
             run = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                platform_velocity[0] -= 1
+                platform.velocity[0] -= 1
                 press = True
             if event.key == pygame.K_RIGHT:
-                platform_velocity[0] += 1
+                platform.velocity[0] += 1
                 press = True
     if not press:
-        if platform_velocity[0] > 0:
-            platform_velocity[0] += VEL_DEGRADE
-        elif platform_velocity[0] < 0:
-            platform_velocity[0] -= VEL_DEGRADE
-    if broken_count == BRICK_COLS*BRICK_ROWS:
-        game_over('W')
+        if platform.velocity[0] > 0:
+            platform.velocity[0] += Platform.VEL_DEGRADE
+        elif platform.velocity[0] < 0:
+            platform.velocity[0] -= Platform.VEL_DEGRADE
+    if grid.empty():
+        window.game_over('W', grid.broken)
         run = False
-    if ball_coords[0] + ball_velocity[0] >= BNDRY_H  or ball_coords[0] + ball_velocity[0] < 0:
-        ball_velocity[0] *= -1
-    if ball_coords[1] + ball_velocity[1] < MARGIN_V:
-        ball_velocity[1] *= -1
-    if ball_coords[1] + ball_velocity[1] >= BNDRY_V:
-        game_over('L')
+    ball.update()
+    platform.update()
+    if ball.coords[1] + ball.velocity[1] >= Ball.BNDRY_V:
+        window.game_over('L', grid.broken)
         run = False
     if run == False:
         break
-    if platform_coords[0] + platform_velocity[0] >= BNDRY_H  or platform_coords[0] + platform_velocity[0] < 0:
-        platform_velocity[0] *= -1
-    ball_coords = ((ball_coords[0] + ball_velocity[0]), (ball_coords[1] + ball_velocity[1]))
-    platform_coords = ((platform_coords[0] + platform_velocity[0]), (platform_coords[1] + platform_velocity[1]))
-    update_bricks(ball_coords)
-    platform_bounce()
+    grid.update(ball=ball)
+    platform.bounce(ball=ball)
     screen.fill((0,0,0))
-    screen.blit(background, (0,0))
-    load_platform(platform_coords)
-    load_ball(ball_coords)
-    load_bricks()
-    load_score(broken_count)
+    window.load(grid.broken)
+    platform.load()
+    ball.load()
+    grid.load()
     pygame.display.update()
